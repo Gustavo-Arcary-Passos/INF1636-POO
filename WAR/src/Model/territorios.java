ad805603f2f170942ab52a3618bfc17a2bd3826c
Gustavo <gustavo.arcary@gmail.com>
2023-10-05 22:46:49 -0300

    Inicio

---
+++ WAR/src/Model/territorios.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class territorios {
+	/*
+	 * Criar uma lista de territorios
+	 * Criar uma area para dizer a qual jogador pertence o territorio
+	 * Qual regiao pertence
+	 * Quantos exercitos possui o territorio
+	 * Quais sao suas fronteiras
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	/*
+	 * Brasil:Argentina,Peru,Venezuela,Nigeria
+	 * Argentina:Brasil,Peru
+	 * Peru:Brasil,Argentina,Venezuela
+	 * Venezuela:Mexico,Brasil,Peru
+	 * Mexico:Venezuela,California,Texas
+	 * California:
+	 * Texas:
+	 */
+}

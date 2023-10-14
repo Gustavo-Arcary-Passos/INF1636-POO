204882ec4e6225627a2f0bd6b167f453aaddb965
Felipe Barcellos <f_barcellos@terra.com.br>
2023-10-14 20:49:43 -0300

    Testando um pouco

--- WAR/src/Model/DeckObjetivos.java
+++ WAR/src/Model/DeckObjetivos.java
@@ -6,7 +6,11 @@ import java.util.*;
  * Uma lista com todos os objetivos do jogo.
  */
 class DeckObjetivos {
-
+	Objetivo1 oi = new Objetivo1();
     private static List<Objetivo> objetivos;
- 
+    
+}
+
+private class Objetivo1 extends Objetivo {
+	 
 }

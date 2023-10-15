c10212329b3b47ac60652c5eb89b4dcb55765e74
Gustavo <gustavo.arcary@gmail.com>
2023-10-14 21:44:35 -0300

    Tirei protected dos bgls

--- WAR/src/Model/DeckObjetivos.java
+++ WAR/src/Model/DeckObjetivos.java
@@ -34,46 +34,46 @@ class DeckObjetivos {
     
 }
 
-protected class Objetivo1 extends Objetivo {
+class Objetivo1 extends Objetivo {
 	//exterminar o azul, se for o azul ou outro jogador tiver eliminado o azul, 24 territorios
 	 public boolean verifica_status() {
 		 
 	 }
 }
-protected class Objetivo2 extends Objetivo {
+class Objetivo2 extends Objetivo {
 	 //exterminar o amarelo, se for o amarelo ou outro jogador tiver eliminado o amarelo, 24 territorios
 }
-protected class Objetivo3 extends Objetivo {
+class Objetivo3 extends Objetivo {
 	//exterminar o branco, se for o branco ou outro jogador tiver eliminado o branco, 24 territorios
 }
-protected class Objetivo4 extends Objetivo {
+class Objetivo4 extends Objetivo {
 	//exterminar o verde, se for o verde ou outro jogador tiver eliminado o verde, 24 territorios 
 }
-protected class Objetivo5 extends Objetivo {
+class Objetivo5 extends Objetivo {
 	//exterminar o preto, se for o preto ou outro jogador tiver eliminado o preto, 24 territorios
 }
-protected class Objetivo6 extends Objetivo {
+class Objetivo6 extends Objetivo {
 	//exterminar o vermelho, se for o vermelho ou outro jogador tiver eliminado o vermelho, 24 territorios
 }
-protected class Objetivo7 extends Objetivo {
+class Objetivo7 extends Objetivo {
 	 //conquistar a américa do Norte e a África
 }
-protected class Objetivo8 extends Objetivo {
+class Objetivo8 extends Objetivo {
 	 //conquistar ásia e africa
 }
-protected class Objetivo9 extends Objetivo {
+class Objetivo9 extends Objetivo {
 	 //conquistar a américa do norte e a oceania
 }
-protected class Objetivo10 extends Objetivo {
+class Objetivo10 extends Objetivo {
 	 //Europa, américa do Sul e continente aleatorio
 }
-protected class Objetivo11 extends Objetivo {
+class Objetivo11 extends Objetivo {
 	 //Asia e America do Sul
 }
-protected class Objetivo12 extends Objetivo {
+class Objetivo12 extends Objetivo {
 	 //Europa Oceania e +1
 }
-protected class Objetivo13 extends Objetivo {
+class Objetivo13 extends Objetivo {
 	 //18 territorios e 2 exercitos em cada
 	public boolean verifica_status() {
 		if(dono.qtd_territorios()>=18) {
@@ -90,7 +90,7 @@ protected class Objetivo13 extends Objetivo {
 		
 	}
 }
-protected class Objetivo14 extends Objetivo {
+class Objetivo14 extends Objetivo {
 	//24 territorios
 	public boolean verifica_status() {
 		if(dono.qtd_territorios()>=24) {

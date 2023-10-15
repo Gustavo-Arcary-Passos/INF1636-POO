f1b7a355de5b479134c4a55d441777fc5203ad43
Felipe Barcellos <f_barcellos@terra.com.br>
2023-10-15 03:00:25 -0300

    Implementação dos objetivos 7 a 14, nao implementei so 1 ao 6

--- WAR/src/Model/DeckObjetivos.java
+++ WAR/src/Model/DeckObjetivos.java
@@ -8,20 +8,20 @@ import java.util.*;
 class DeckObjetivos {
     private List<Objetivo> objetivos;
     
-    
-    public DeckObjetivos() {
+    //Temos que passar uma referência aos continentes criados para verificar alguns objetivos
+    public DeckObjetivos(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa) {
     	objetivos.add(new Objetivo1());
     	objetivos.add(new Objetivo2());
     	objetivos.add(new Objetivo3());
     	objetivos.add(new Objetivo4());
     	objetivos.add(new Objetivo5());
     	objetivos.add(new Objetivo6());
-    	objetivos.add(new Objetivo7());
-    	objetivos.add(new Objetivo8());
-    	objetivos.add(new Objetivo9());
-    	objetivos.add(new Objetivo10());
-    	objetivos.add(new Objetivo11());
-    	objetivos.add(new Objetivo12());
+    	objetivos.add(new Objetivo7(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa));
+    	objetivos.add(new Objetivo8(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa));
+    	objetivos.add(new Objetivo9(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa));
+    	objetivos.add(new Objetivo10(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa));
+    	objetivos.add(new Objetivo11(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa));
+    	objetivos.add(new Objetivo12(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa));
     	objetivos.add(new Objetivo13());
     	objetivos.add(new Objetivo14());
     }
@@ -54,24 +54,98 @@ class Objetivo5 extends Objetivo {
 }
 class Objetivo6 extends Objetivo {
 	//exterminar o vermelho, se for o vermelho ou outro jogador tiver eliminado o vermelho, 24 territorios
+	public boolean verifica_status() {
+		//nao sei como implementar
+	}
 }
 class Objetivo7 extends Objetivo {
 	 //conquistar a américa do Norte e a África
+	public boolean verifica_status() {
+		if(americanorte.verifica_monopolio(dono)) {
+			if(africa.verifica_monopolio(dono)) {
+				return true;
+			}
+		}	
+		return false;
+	}
 }
 class Objetivo8 extends Objetivo {
 	 //conquistar ásia e africa
+	public boolean verifica_status() {
+		if(asia.verifica_monopolio(dono)) {
+			if(africa.verifica_monopolio(dono)) {
+				return true;
+			}
+		}	
+		return false;
+	}
 }
 class Objetivo9 extends Objetivo {
 	 //conquistar a américa do norte e a oceania
+	public boolean verifica_status() {
+		if(americanorte.verifica_monopolio(dono)) {
+			if(oceania.verifica_monopolio(dono)) {
+				return true;
+			}
+		}	
+		return false;
+	}
 }
 class Objetivo10 extends Objetivo {
 	 //Europa, américa do Sul e continente aleatorio
+	public boolean verifica_status() {
+		if(europa.verifica_monopolio(dono)) {
+			if(americasul.verifica_monopolio(dono)) {
+				if(asia.verifica_monopolio(dono)) {
+					return true;
+				}
+				if(oceania.verifica_monopolio(dono)) {
+					return true;
+				}
+				if(americanorte.verifica_monopolio(dono)) {
+					return true;
+				}
+				if(africa.verifica_monopolio(dono)) {
+					return true;
+				}
+			}
+		}	
+		return false;
+	}
 }
 class Objetivo11 extends Objetivo {
 	 //Asia e America do Sul
+	public boolean verifica_status() {
+		if(asia.verifica_monopolio(dono)) {
+			if(americasul.verifica_monopolio(dono)) {
+				return true;
+			}
+		}	
+		return false;
+	}
 }
 class Objetivo12 extends Objetivo {
 	 //Europa Oceania e +1
+	public boolean verifica_status() {
+		if(europa.verifica_monopolio(dono)) {
+			if(oceania.verifica_monopolio(dono)) {
+				if(asia.verifica_monopolio(dono)) {
+					return true;
+				}
+				if(americasul.verifica_monopolio(dono)) {
+					return true;
+				}
+				if(americanorte.verifica_monopolio(dono)) {
+					return true;
+				}
+				if(africa.verifica_monopolio(dono)) {
+					return true;
+				}
+			}
+		}	
+		return false;
+	}
+	
 }
 class Objetivo13 extends Objetivo {
 	 //18 territorios e 2 exercitos em cada
--- WAR/src/Model/Objetivo.java
+++ WAR/src/Model/Objetivo.java
@@ -2,9 +2,18 @@ package Model;
 
 abstract class Objetivo {
 	protected Jogador dono;
+	protected Regiao asia, europa, americasul, americanorte, africa, oceania;
     // construtor
     public Objetivo() {
        
+    }
+    public Objetivo(Regiao asia, Regiao oceania, Regiao africa, Regiao americasul, Regiao americanorte, Regiao europa) {
+    	this.asia = asia;
+    	this.oceania = oceania;
+    	this.africa = africa;
+    	this.americasul = americasul;
+    	this.americanorte = americanorte;
+    	this.europa = europa;
     }
     
     // roda programa para saber se um jogador ganhou

1b7121c45f1a1f011e5fb1e82ff6b3cc55a85513
Gustavo <gustavo.arcary@gmail.com>
2023-10-15 13:50:07 -0300

    Mais testes do Junit Jogador

--- WAR/Teste/Model/TesteJogador.java
+++ WAR/Teste/Model/TesteJogador.java
@@ -2,6 +2,9 @@ package Model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class TesteJogador {
@@ -14,6 +17,33 @@ public class TesteJogador {
 		assertEquals("Azul", jogador.get_cor());
 	}
 
-	//TODO: teste territórios, com o novo sistema
+	@Test
+	public void teste_conquista_territorio()
+	{
+		Jogador jogador = new Jogador("Jogador", "Azul");
+		Jogador jogador2 = new Jogador("Joel Lambretinha", "Preto");
+		Territorio BrasilTeste = new Territorio("Brasil", "América do Sul", new ArrayList<>(Arrays.asList("Argentina", "Peru", "Venezuela", "Nigéria")));
+		BrasilTeste.set_Jogador(jogador);
+		jogador.ganha_territorio(BrasilTeste);
+		assertEquals(jogador.qtd_territorios(),1);
+		assertEquals(jogador2.qtd_territorios(),0);
+		jogador.perde_territorio(BrasilTeste);
+		BrasilTeste.set_Jogador(jogador2);
+		jogador2.ganha_territorio(BrasilTeste);
+		assertEquals(jogador.qtd_territorios(),0);
+		assertEquals(jogador2.qtd_territorios(),1);
+	}
+	
+	@Test
+	public void teste_troca_cartas_exercito()
+	{
+		
+	}
+	
+	@Test
+	public void teste_ganha_exercito_inicio_rodada()
+	{
+		
+	}
 
 }

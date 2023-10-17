ea01f1e49a9851d225f3730ba928fc8e71df2296
Gustavo <gustavo.arcary@gmail.com>
2023-10-16 21:33:32 -0300

    Coloquei imagens

--- WAR/src/View/Tela.java
+++ WAR/src/View/Tela.java
@@ -10,12 +10,18 @@ public class Tela extends JFrame {
 		setSize(LARG_DEFAULT,ALT_DEFAULT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		getContentPane().setBackground(new Color(0, 0, 0));
-		ImageIcon imagemIcon = new ImageIcon(""); // Substitua pelo caminho da sua imagem
+		ImageIcon imagemIcon = new ImageIcon("WAR-Imagens/images/war_tabuleiro_fundo.png");
         Image imagem = imagemIcon.getImage();
+        int larguraNova = 768; // Largura desejada
+        int alturaNova = 576;
+        ImageIcon mapa_mundo = new ImageIcon("WAR-Imagens/images/war_tabuleiro_mapacopy.png");
+        Image mapa = mapa_mundo.getImage();
+        
 
         // Criando um JLabel para exibir a imagem
         JLabel imagemLabel = new JLabel(new ImageIcon(imagem));
+        JLabel mapaLabel = new JLabel(new ImageIcon(mapa));
         add(imagemLabel); // Adiciona o JLabel ao JFrame
-
+        add(mapaLabel);
 	}
 }

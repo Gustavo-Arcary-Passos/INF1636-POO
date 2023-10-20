35c20a2232907fe08abcba49901b493a0b1c8d57
zodecky <gzagury@aluno.puc-rio.br>
2023-10-20 12:51:17 -0300

    comentarios adicionados

--- WAR/src/View/Main.java
+++ WAR/src/View/Main.java
@@ -13,11 +13,13 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Tela frame = buildFrame();
+        Tela frame = buildFrame(); // função para inicializar o frame
 
+		// Imagens que vão ser carregadas:
         final BufferedImage plano_de_fundo = ImageIO.read(new File("WAR/src/View/imagens/war_tabuleiro_fundo.png"));
 		final BufferedImage war_mapa = ImageIO.read(new File("WAR/src/View/imagens/war_tabuleiro_mapacopy.png"));
 
+		// Desenhando as imagens:
         JPanel pane = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
@@ -27,21 +29,31 @@ public class Main {
             }
         };
 
-
+		// Adiciona o painel ao frame
         frame.add(pane);
     }
 
 
+	/**
+	 * @brief Função para criar um frame
+	 * configurações do mesmo estarão presentes aqui
+	 * @return
+	 */
     private static Tela buildFrame() {
         Tela frame = new Tela();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(1024, 768);
         frame.setVisible(true);
-		frame.addComponentListener(new ResizeListener());
+		frame.addComponentListener(new ResizeListener()); // event listener resize
         return frame;
     }
 }
 
+/**
+ * Classe para saber se o tamanho do frame foi alterado,
+ * no caso pode ser utilizada com qualquer componente (é um event listener)
+ * e precisa ser adicionado em cada manualmente. É utilizado na função buildFrame()
+ */
 class ResizeListener extends ComponentAdapter {
         public void componentResized(ComponentEvent e) {
             System.out.println("mudou o tamanho da janela");

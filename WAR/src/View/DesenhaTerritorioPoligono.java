5af11d2ea2ae868b6f972919be6cc1a8fc54e552
Gustavo <gustavo.arcary@gmail.com>
2023-10-20 21:47:53 -0300

    Add metodo para colocar poligonos nos territorios

---
+++ WAR/src/View/DesenhaTerritorioPoligono.java
@@ -0,0 +1,83 @@
+package View;
+
+import java.awt.Color;
+import java.awt.geom.Path2D;
+
+public class DesenhaTerritorioPoligono {
+	protected String nome;
+	protected Color cor;
+	protected Path2D terra;
+	
+	public DesenhaTerritorioPoligono (int[] xs, int[] ys,Color color,String name) {
+		this.cor = color;
+		this.nome = name;
+		this.terra = new Path2D.Double();
+		for(int i = 0; i < xs.length ; i++) {
+			if(i == 0) {
+				this.terra.moveTo(xs[i], ys[i]);
+			} else {
+				this.terra.lineTo(xs[i], ys[i]);
+			}
+		}
+		this.terra.closePath();
+	}
+	
+	public String get_nome() {
+		return this.nome;
+	}
+	
+	public Color get_cor() {
+		return this.cor;
+	}
+	
+	public Path2D get_polygon() {
+		return this.terra;
+	}
+	
+	public void set_color(Color cor_atual) {
+		this.cor = cor_atual;
+	}
+	
+	public boolean clicou(int x, int y) {
+		if(this.terra.contains(x, y) == true) {
+			System.out.println("O ponto está dentro do polígono.");
+			return true;
+		}
+		System.out.println("O ponto está fora do polígono.");
+		return false;
+	}
+}
+
+//Graphics2D g2d = (Graphics2D) g;
+//
+//// Criar um polígono usando Path2D
+//Path2D polygon = new Path2D.Double();
+//polygon.moveTo(274, 500); 
+//polygon.lineTo(332, 500); 
+//polygon.lineTo(326, 487); 
+//polygon.lineTo(343, 457);
+//polygon.lineTo(336, 441);
+//polygon.lineTo(315, 441);
+//polygon.lineTo(306, 421);
+//polygon.lineTo(291, 418);
+//polygon.lineTo(285, 402);
+//polygon.lineTo(254, 394);
+//polygon.lineTo(234, 430);
+//polygon.closePath(); // Feche o polígono
+//
+//boolean pontoDentro = polygon.contains(301, 426);
+//
+//if (pontoDentro) {
+//    System.out.println("O ponto está dentro do polígono.");
+//} else {
+//    System.out.println("O ponto está fora do polígono.");
+//}
+//
+//// Defina a cor de preenchimento e preencha o polígono
+//Color minhaCor = new Color(255, 0, 255);
+//g2d.setColor(minhaCor);
+//g2d.fill(polygon);
+//
+//// Defina a cor da linha e desenhe o contorno do polígono
+//g2d.setColor(Color.BLACK);
+//g2d.draw(polygon);
--- WAR/src/View/Tela.java
+++ WAR/src/View/Tela.java
@@ -1,6 +1,8 @@
 package View;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +15,8 @@ class Tela extends JFrame {
 	protected final int LARG_DEFAULT=1200;
 	protected final int ALT_DEFAULT=800;
 	private ImagemInfo[] images;
-	private int count;
+	private DesenhaTerritorioPoligono[] terras;
+	private int count_images,count_terras;
 	public Tela() {
 		setSize(LARG_DEFAULT,ALT_DEFAULT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -21,21 +24,39 @@ class Tela extends JFrame {
 		images = new ImagemInfo[100];
 		count_images_loaded(new ImagemInfo ("war_tabuleiro_fundo.png",0,0,1024,768));
 		count_images_loaded(new ImagemInfo ("war_tabuleiro_mapa copy.png",0,0,1024,768));
+		
+		terras = new DesenhaTerritorioPoligono[51];
+		count_terras_loaded(new DesenhaTerritorioPoligono(new int[]{274,332,326,343,336,315,306,291,285,254,234},new int[]{500,500,487,457,441,441,421,418,402,394,430}, Color.RED, "Brasil"));
+		
 	}
 	
 	@Override
 	public void paint(Graphics g) {
 	    super.paint(g);
-	    for(int i = 0; i <= this.count; i++) {
+	    for(int i = 0; i <= this.count_images; i++) {
 		    if (images[i] != null) {
 		        g.drawImage(images[i].get_image(), images[i].get_x(), images[i].get_y(),images[i].get_w(),images[i].get_h(), this);
+		    }
+	    }
+	    Graphics2D g2d = (Graphics2D) g;
+	    for(int i = 0; i <= this.count_terras; i++) {
+		    if (terras[i] != null) {
+		    	g2d.setColor(terras[i].get_cor());
+		    	g2d.fill(terras[i].get_polygon());
+		    	g2d.setColor(Color.BLACK);
+		    	g2d.draw(terras[i].get_polygon());
 		    }
 	    }
 	}
 	
 	public void count_images_loaded(ImagemInfo imagem) {
-		count++;
-		this.images[count-1] = imagem;
+		count_images++;
+		this.images[count_images-1] = imagem;
+	}
+	
+	public void count_terras_loaded(DesenhaTerritorioPoligono terra) {
+		count_terras++;
+		this.terras[count_terras-1] = terra;
 	}
 }
 

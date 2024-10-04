037b75aff93e48c96b881ea220e58e46b5b1b5c2
zodecky <103199986+zodecky@users.noreply.github.com>
2024-10-04 15:08:41 -0300

    kjh

--- WAR/src/Model/AmericadoSul.java
+++ WAR/src/Model/AmericadoSul.java
@@ -7,7 +7,7 @@ class AmericadoSul extends Regiao {
 	public AmericadoSul() {
 		this.nome = "América do Sul";
 		this.exercito_extra = 2;
-		
+		paises.add(new Territorio("Brasil", "América do Sul", new ArrayList<>(Arrays.asList("Argentina", "Peru", "Venezuela", "Nigéria"))));
         paises.add(new Territorio("Argentina", "América do Sul", new ArrayList<>(Arrays.asList("Brasil", "Peru"))));
         paises.add(new Territorio("Peru", "América do Sul", new ArrayList<>(Arrays.asList("Brasil", "Argentina", "Venezuela"))));
         paises.add(new Territorio("Venezuela", "América do Sul", new ArrayList<>(Arrays.asList("México", "Brasil", "Peru"))));
